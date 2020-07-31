b6db3914521de6ce18b85b13c8b228dd9171aa88
Frank Dong <frank.dong2906@gmail.com>
2020-07-31 01:21:49 +0100

    Rebuild coin flip function to take 2 parameters and add result to money, add 'cho-han' game, follow same layout as coin flip game.

--- games.py
+++ games.py
@@ -4,43 +4,84 @@ money = 100
 
 #Write your game of chance functions here
 
-def flip_coin(bet, choice):
-    if money > 0 and money >= bet:
-        num = random.randint(1, 2)
-        money -= bet
-        if num == 1 and choice == 'Heads':
-            print('You have betted ' + str(bet) + ' on this game!\n' + 'You currently have ' + str(money) +  ' cash.\n\n' + 'You picked heads!\n' + 'The outcome was heads!') 
-            bet *= 2
-            money += bet
-            print('You have won the bet! You earned ' + str(bet) + '! \n\nYou currently have ' + str(money) + ' cash.')
-            return money
+def coin_flip(bet, guess): 
+    if bet < 0:
+        print("Your bet must be greater than zero. Please try again.\n")
 
-        elif num == 1 and choice == 'Tails':
-            print('You have betted ' + str(bet) + ' on this game!\n' + 'You currently have ' + str(money) +  ' cash.\n\n' + 'You picked heads!\n' + 'The outcome was tails!') 
-            print('You lost the bet! You lost ' + str(bet) + '! \n\nYou currently have ' + str(money) + ' cash.')
-            return money
-        
-        elif num == 2 and choice == 'Heads':
-            print('You have betted ' + str(bet) + ' on this game!\n' + 'You currently have ' + str(money) +  ' cash.\n\n' + 'You picked tails!\n' + 'The outcome was heads!') 
-            print('You lost the bet! You lost ' + str(bet) + '! \n\nYou currently have ' + str(money) + ' cash.')
-            return money
+    elif bet > money:
+        print("You don't have that much money. Please try again.\n")
 
-        elif num == 2 and choice == 'Tails':
-            print('You have betted ' + str(bet) + ' on this game!\n' + 'You currently have ' + str(money) +  ' cash.\n\n' + 'You picked tails!\n' + 'The outcome was heads!') 
-            bet *= 2
-            money += bet
-            print('You have won the bet! You earned ' + str(bet) + '! \n\nYou currently have ' + str(money) + ' cash.')
-            return money
     else:
-        print('You do not have enough cash for this bet!')
-            
+        num = random.randint(1, 2)
+        if num == 1 and guess == 'Heads':
+            print('You have betted ' + str(bet) + ' on this game!\n\n'+ 'You guessed heads!\n' + 'The outcome was heads!') 
+            bet *= 1
+            print('You won the bet! You earned ' + str(bet) + '!')
+            return bet
+
+        elif num == 1 and guess == 'Tails':
+            print('You have betted ' + str(bet) + ' on this game!\n\n'+ 'You guessed heads!\n' + 'The outcome was tails!') 
+            bet *= -1
+            print('You lost the bet! You lost ' + str(bet) + '!')
+            return bet
+
+        elif num == 2 and guess == 'Heads':
+            print('You have betted ' + str(bet) + ' on this game!\n\n'+ 'You guessed tails!\n' + 'The outcome was heads!') 
+            bet *= -1
+            print('You lost the bet! You lost ' + str(bet) + '!')
+            return bet
+
+        elif num == 2 and guess == 'Tails':
+            print('You have betted ' + str(bet) + ' on this game!\n\n'+ 'You guessed tails!\n' + 'The outcome was tails!') 
+            bet *= 1
+            print('You won the bet! You earned ' + str(bet) + '!')
+            return bet
+
+
+
+def cho_han(bet, guess):
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    num = (dice1 + dice2) % 2
+     
+    if bet < 0:
+        print("Your bet must be greater than zero. Please try again.\n")
+
+    elif bet > money:
+        print("You don't have that much money. Please try again.\n")
+
+    else:
+        if num != 0 and guess == 'Odd':
+            print('You have betted ' + str(bet) + ' on this game!\n\n'+ 'You guessed odd!\n' + 'The outcome was odd!') 
+            bet *= 1
+            print('You won the bet! You earned ' + str(bet) + '!')
+            return bet
+        
+        elif num != 0 and guess == 'Even':
+            print('You have betted ' + str(bet) + ' on this game!\n\n'+ 'You guessed even!\n' + 'The outcome was odd!') 
+            bet *= -1
+            print('You lost the bet! You lost ' + str(bet) + '!')
+            return bet
+        
+        elif num == 0 and guess == 'Odd':
+            print('You have betted ' + str(bet) + ' on this game!\n\n'+ 'You guessed odd!\n' + 'The outcome was even!') 
+            bet *= -1
+            print('You lost the bet! You lost ' + str(bet) + '!')
+            return bet
+
+        elif num == 0 and guess == 'Even':
+            print('You have betted ' + str(bet) + ' on this game!\n\n'+ 'You guessed even!\n' + 'The outcome was even!') 
+            bet *= 1
+            print('You won the bet! You won ' + str(bet) + '!')
+            return bet
+        
+
+
+def cash_check():
+    print('You have ' + str(money) + ' cash.')
 
 
 #Call your game of chance functions here
-flip_coin(50, 'Heads', money)
-
-def cho_han(bet, choice, money):
-    if money > 0 and money >= bet:
-        num = random.randint(1, 6)
-    else:
-        print('You do not have enough cash for this bet!')
+money += coin_flip(50, 'Heads')
+money += cho_han(50, 'Odd')
+cash_check()
